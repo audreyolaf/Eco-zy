c643ea6167823bc1931d301e2646e24632ee16d7
Audrey Wang <62197228+audreyolaf@users.noreply.github.com>
2021-08-04 11:13:10 -0700

    aesthetic change
    dr strange

--- Eco-zy/ViewController.swift
+++ Eco-zy/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var homepageTitle: UILabel!
+    @IBOutlet weak var aboutUsButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        homepageTitle.layer.cornerRadius = 15
+        aboutUsButton.layer.cornerRadius = 15
     }
 
 
