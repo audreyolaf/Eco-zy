0a84ab18c1a8202847d971a5824e19932da98a40
anushka rane <anushkarane2018@gmail.com>
2021-08-05 23:56:14 -0700

    finally worked!!!!

--- Eco-zy/CheckmarkViewController.swift
+++ Eco-zy/CheckmarkViewController.swift
@@ -13,8 +13,16 @@ class CheckmarkViewController: UIViewController {
     @IBOutlet weak var check1: UIButton!
     @IBOutlet weak var check2: UIButton!
     @IBOutlet weak var check3: UIButton!
+    @IBOutlet weak var check4: UIButton!
+    @IBOutlet weak var check5: UIButton!
+    @IBOutlet weak var check6: UIButton!
     var isChecked1: Bool = false
     var isChecked2: Bool = false
+    var isChecked3: Bool = false
+    var isChecked4: Bool = false
+    var isChecked5: Bool = false
+    var isChecked6: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,6 +43,38 @@ class CheckmarkViewController: UIViewController {
             check2.setImage(UIImage(named: "icons8-ok-30"), for: .normal)
         } else {
             isChecked2 = false
+        }
+    }
+    @IBAction func checkTap3(_ sender: Any) {
+        isChecked3 = true
+        if isChecked3 {
+            check3.setImage(UIImage(named: "icons8-ok-30"), for: .normal)
+        } else {
+            isChecked3 = false
+        }
+    }
+    @IBAction func checkTap4(_ sender: Any) {
+        isChecked4 = true
+        if isChecked4 {
+            check4.setImage(UIImage(named: "icons8-ok-30"), for: .normal)
+        } else {
+            isChecked4 = false
+        }
+    }
+    @IBAction func checkTap5(_ sender: Any) {
+        isChecked5 = true
+        if isChecked5 {
+            check5.setImage(UIImage(named: "icons8-ok-30"), for: .normal)
+        } else {
+            isChecked5 = false
+        }
+    }
+    @IBAction func checkTap6(_ sender: Any) {
+        isChecked6 = true
+        if isChecked6 {
+            check6.setImage(UIImage(named: "icons8-ok-30"), for: .normal)
+        } else {
+            isChecked6 = false
         }
     }
     
