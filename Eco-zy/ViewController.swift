6fd384a5ecd9e492c2c92361cdf6c1c816349b7e
Maliha <Malihas2005@gmail.com>
2021-08-06 11:19:49 -0700

    final push

--- Eco-zy/ViewController.swift
+++ Eco-zy/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 //    @IBOutlet weak var homepageTitle: UILabel!
 //    @IBOutlet weak var aboutUsButton: UIButton!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
